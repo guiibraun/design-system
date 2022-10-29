806883990bd8b7d5dc6692405ae600d41f6539a5
Guilherme Braun <guiabraun@hotmail.com>
2022-10-29 18:52:02 -0300

    fix: Resolving issues

--- packages/docs/src/stories/TextArea.stories.tsx
+++ packages/docs/src/stories/TextArea.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, TextArea, TextAreaProps, Text } from '@ignite-ui/react'
+import { Box, TextArea, TextAreaProps, Text } from '@gui-ignite-ui/react'
 
 export default {
   title: 'Form/Text Area',
